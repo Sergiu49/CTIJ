9ddafbe8a432dc2836a04adf67927261c00a2785
zSnowflake17 <denisa.bizic@student.upt.ro>
2026-01-16 20:02:07 +0200

    Dialogue System
    Ce e aici este partea de dialog la interactiunea cu un NPC

---
+++ Assets/Scripts/Character/NPCController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour, Interactable
+{
+
+    [SerializeField] Dialog dialog;
+    public void Interact()
+    {
+        
+        StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+        
+    }
+    
+}
