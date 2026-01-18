ca7928a51e6b8b4e99d3f75fa1aa66d412c34969
zSnowflake17 <denisa.bizic@student.upt.ro>
2026-01-18 17:40:58 +0200

    Am facut portale si cu un mic Indoor scene sa intri intr-o casuta

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -18,6 +18,14 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         this.player = player;
         StartCoroutine(SwitchScene());
     }
+
+    Fader fader;
+    private void Start()
+    {
+
+       fader = FindObjectOfType<Fader>();
+
+    }
     
     IEnumerator SwitchScene()
     {
@@ -25,12 +33,14 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         DontDestroyOnLoad(gameObject); //sa nu distrugem portalul cand incarcam scena
         
         GameControler.Instance.PauseGame(true);
+        yield return fader.FadeIn(0.5f);
         
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
         
         var destPortal=FindObjectsOfType<Portal>().First(x => x!=this && x.destinationPortal==this.destinationPortal);
         player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
         
+        yield return fader.FadeOut(0.5f);
         GameControler.Instance.PauseGame(false);
         
         Destroy(gameObject);
